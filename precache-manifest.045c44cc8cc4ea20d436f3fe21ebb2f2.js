3be77a3e29d9bfcd408ce471f137b19b139017cd
Allwyn Sequeira <allwyn@gmail.com>
2019-11-24 05:02:23 -0800

    Updates

---
+++ precache-manifest.045c44cc8cc4ea20d436f3fe21ebb2f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea4efb0e39fdce942d423a034961f26e",
+    "url": "/adrive/index.html"
+  },
+  {
+    "revision": "7a6ffff4b8a878d0c403",
+    "url": "/adrive/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "0984a07fa51b8c5d04b5",
+    "url": "/adrive/static/js/2.ff1385b1.chunk.js"
+  },
+  {
+    "revision": "7a6ffff4b8a878d0c403",
+    "url": "/adrive/static/js/main.56b99f12.chunk.js"
+  },
+  {
+    "revision": "b06bdddfbadc53eac512",
+    "url": "/adrive/static/js/runtime-main.a812b583.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/adrive/static/media/logo.25bf045c.svg"
+  }
+]);
